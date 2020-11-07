a3af4c41a45754c20665be753b879ed4ddb51be7
Damla Koksal <damlakoksal@outlook.com>
2020-11-07 19:24:36 +0300

    feat: styled navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./navbar.styles.scss";
 import logo from "../../logo.svg";
 
-export default function Nav() {
+export default function NavBar() {
   return (
     <div className="navbar-container">
-      <img src={logo} className="App-logo" alt="logo" />
-      <div className="link-container">
+      <div className="link-container left">
+        <Link to="/">test</Link>
+        <Link to="/">test</Link>
+        <Link to="/">test</Link>
+      </div>
+      <img src={logo} className="App-logo" alt="logo" width={150} height={75} />
+      <div className="link-container right">
         <Link to="/">test</Link>
         <Link to="/">test</Link>
         <Link to="/">test</Link>
