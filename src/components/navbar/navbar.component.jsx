31555521564579900b83bf32629567c5a5f690aa
Damla Koksal <damlakoksal@outlook.com>
2020-11-07 20:07:50 +0300

    feat: restyled navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -12,7 +12,7 @@ export default function NavBar() {
         <Link to="/">test</Link>
         <Link to="/">test</Link>
       </div>
-      <img src={logo} className="App-logo" alt="logo" width={150} height={75} />
+      <img src={logo} className="logo" alt="logo" width={150} height={75} />
       <div className="link-container right">
         <Link to="/">test</Link>
         <Link to="/">test</Link>
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -4,14 +4,19 @@ import NavBar from "../components/navbar/navbar.component";
 import Announcement from "../components/announcement/announcement.component";
 import { Link } from "react-router-dom";
 
+import "./homepage.styles.scss";
+
 export default function HomePage() {
   return (
-    <div className="container">
-      <Announcement>
-        <Link to="mailto:me@damlakoksal.com">me@damlakoksal.com</Link>
-        <Link to="tel:+902122222222">+90 (212) 222 22 22</Link>
-      </Announcement>
-      <NavBar />
-    </div>
+    <>
+      <div className="container">
+        <Announcement>
+          <Link to="mailto:me@damlakoksal.com">me@damlakoksal.com</Link>
+          <Link to="tel:+902122222222">+90 (212) 222 22 22</Link>
+        </Announcement>
+        <NavBar />
+      </div>
+      <div className="container pink"></div>
+    </>
   );
 }
